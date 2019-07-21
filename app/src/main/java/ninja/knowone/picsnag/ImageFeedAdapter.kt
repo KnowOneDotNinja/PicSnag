721d39a03bd484cf4dd3baf0acbf79e0a6c74121
Michael W <michael@knowone.ninja>
2019-07-20 18:39:23 -0700

    Live view

--- app/src/main/java/ninja/knowone/picsnag/ImageFeedAdapter.kt
+++ app/src/main/java/ninja/knowone/picsnag/ImageFeedAdapter.kt
@@ -6,11 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class ImageFeedAdapter(val picurls: ArrayList<String>, val context: Context): RecyclerView.Adapter<ImageFeedAdapter.ViewHolder>() {
 
     class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
-
+        var index = 0
+        var ivMain = view.findViewById<ImageView>(R.id.ivMain)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -21,6 +23,8 @@ class ImageFeedAdapter(val picurls: ArrayList<String>, val context: Context): Re
     override fun getItemCount() = picurls.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
+        val picurl = picurls[position]
+        holder.index = position
+        Glide.with(context).load(picurl).into(holder.ivMain)
     }
 }
--- app/src/main/java/ninja/knowone/picsnag/MainActivity.kt
+++ app/src/main/java/ninja/knowone/picsnag/MainActivity.kt
@@ -2,11 +2,34 @@ package ninja.knowone.picsnag
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvMain.adapter = ImageFeedAdapter(picSnag(), this)
+        rvMain.setHasFixedSize(true)
+    }
+
+    fun picSnag(): ArrayList<String> {
+        val picList = ArrayList<String>()
+        picList.add("https://homepages.cae.wisc.edu/~ece533/images/airplane.png")
+        picList.add("https://homepages.cae.wisc.edu/~ece533/images/airplane.png")
+        picList.add("https://homepages.cae.wisc.edu/~ece533/images/airplane.png")
+        picList.add("https://homepages.cae.wisc.edu/~ece533/images/airplane.png")
+        picList.add("https://homepages.cae.wisc.edu/~ece533/images/airplane.png")
+        picList.add("https://homepages.cae.wisc.edu/~ece533/images/airplane.png")
+        picList.add("https://homepages.cae.wisc.edu/~ece533/images/airplane.png")
+        picList.add("https://homepages.cae.wisc.edu/~ece533/images/airplane.png")
+        picList.add("https://homepages.cae.wisc.edu/~ece533/images/airplane.png")
+        picList.add("https://homepages.cae.wisc.edu/~ece533/images/airplane.png")
+        picList.add("https://homepages.cae.wisc.edu/~ece533/images/airplane.png")
+        picList.add("https://homepages.cae.wisc.edu/~ece533/images/airplane.png")
+        picList.add("https://homepages.cae.wisc.edu/~ece533/images/airplane.png")
+        picList.add("https://homepages.cae.wisc.edu/~ece533/images/airplane.png")
+        return picList
     }
 }
